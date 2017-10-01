845796ef0563fcea88209b9fc6d6ca9c1abd13a5
wcars05 <wcars05@BEMOTH>
2017-10-01 17:38:44 -0400

    Add project files.

---
+++ TrickyVR/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TrickyVR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.pictureBoxL = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR = new System.Windows.Forms.PictureBox();
+            this.buttonleft = new System.Windows.Forms.Button();
+            this.buttonright = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(13, 13);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(95, 13);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // pictureBoxL
+            // 
+            this.pictureBoxL.Location = new System.Drawing.Point(37, 153);
+            this.pictureBoxL.MaximumSize = new System.Drawing.Size(960, 540);
+            this.pictureBoxL.Name = "pictureBoxL";
+            this.pictureBoxL.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxL.TabIndex = 2;
+            this.pictureBoxL.TabStop = false;
+            // 
+            // pictureBoxR
+            // 
+            this.pictureBoxR.Location = new System.Drawing.Point(523, 153);
+            this.pictureBoxR.MaximumSize = new System.Drawing.Size(960, 540);
+            this.pictureBoxR.Name = "pictureBoxR";
+            this.pictureBoxR.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxR.TabIndex = 3;
+            this.pictureBoxR.TabStop = false;
+            // 
+            // buttonleft
+            // 
+            this.buttonleft.Location = new System.Drawing.Point(255, 13);
+            this.buttonleft.Name = "buttonleft";
+            this.buttonleft.Size = new System.Drawing.Size(75, 23);
+            this.buttonleft.TabIndex = 4;
+            this.buttonleft.Text = "Left";
+            this.buttonleft.UseVisualStyleBackColor = true;
+            this.buttonleft.Click += new System.EventHandler(this.buttonleft_Click);
+            // 
+            // buttonright
+            // 
+            this.buttonright.Location = new System.Drawing.Point(391, 13);
+            this.buttonright.Name = "buttonright";
+            this.buttonright.Size = new System.Drawing.Size(75, 23);
+            this.buttonright.TabIndex = 5;
+            this.buttonright.Text = "Right";
+            this.buttonright.UseVisualStyleBackColor = true;
+            this.buttonright.Click += new System.EventHandler(this.buttonright_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1111, 513);
+            this.Controls.Add(this.buttonright);
+            this.Controls.Add(this.buttonleft);
+            this.Controls.Add(this.pictureBoxR);
+            this.Controls.Add(this.pictureBoxL);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonExit);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TrickVR";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.PictureBox pictureBoxL;
+        private System.Windows.Forms.PictureBox pictureBoxR;
+        private System.Windows.Forms.Button buttonleft;
+        private System.Windows.Forms.Button buttonright;
+    }
+}
+
---
+++ TrickyVR/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrickyVR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            // show a file select dialog to load from
+            OpenFileDialog imageSelector = new OpenFileDialog();
+            ;
+            if(imageSelector.ShowDialog() == DialogResult.OK) {
+                // load the vr image to show
+
+                // need to then get the image dim and stretch it to a size for viewing and position the two view ports
+                Image img = Image.FromFile(imageSelector.FileName);
+                pictureBoxL.Image = img;
+                pictureBoxR.Image = img;
+
+                // then set the position information  of where they need to go in the window
+                 
+                //pictureBoxL.Height;
+                //;
+                pictureBoxL.Location = new Point(this.Width/4 - pictureBoxL.Width/2, this.Height/2 - pictureBoxL.Height/2);// = 100;
+                pictureBoxR.Location = new Point(this.Width / 4 + this.Width / 2 - pictureBoxL.Width / 2, this.Height / 2 - pictureBoxL.Height / 2);// = 100;
+                //pictureBoxL.Location.Y = 100;
+                // might need some red alignment indicators at the top center of the magiceyes
+
+
+            }
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // use this to move the right picture left and right depending on the key
+            Console.WriteLine(e.KeyChar.ToString());
+        }
+
+        private void buttonleft_Click(object sender, EventArgs e)
+        {
+            pictureBoxR.Location = new Point(pictureBoxR.Location.X-1, pictureBoxR.Location.Y);
+        }
+
+        private void buttonright_Click(object sender, EventArgs e)
+        {
+            pictureBoxR.Location = new Point(pictureBoxR.Location.X + 1, pictureBoxR.Location.Y);
+        }
+    }
+}
+
